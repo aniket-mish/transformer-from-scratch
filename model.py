7c1b7dbc43805106e4e86dca233e3fa5f66d13ff
Aniket Mishrikotkar <aniketmishrikotkar@gmail.com>
2023-10-29 15:37:25 +0530

    add layer normalization module

--- model.py
+++ model.py
@@ -35,3 +35,15 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + self.positional_encoding[:, :x.size(1)].requires_grad_(False) # shape and size returns the same thing
         return self.dropout(x)
+
+class LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) # learnable parameter
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True) # -1 means the last dimension
+        std = x.std(-1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias

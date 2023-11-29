101a7bec0254425e173d4b589f9c4a2562e48fd9
Aniket Mishrikotkar <aniketmishrikotkar@gmail.com>
2023-11-29 21:36:20 +0530

    add comments

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ class InputEmbeddings(nn.Module):
         super().__init__()
         self.d_model = d_model
         self.vocab_size = vocab_size
-        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.embedding = nn.Embedding(vocab_size, d_model) # dictionary kind of layer that maps the input to a vector of size d_model
         
     def forward(self, input):
         return self.embedding(input) * math.sqrt(self.d_model)
@@ -17,14 +17,17 @@ class PositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.d_model = d_model
         self.seq_len = seq_len # max len of the sentences
-        self.register_buffer('positional_encoding', self._get_positional_encoding()) # lets you save and restore tensors that are not model parameters but are still part of the model state.
+
+        # lets you save and restore tensors that are not model parameters but are still part of the model state.
+        # for example, a buffer that should not be updated during backpropagation using gradient descent but should be a part of the model's state.
+        self.register_buffer('positional_encoding', self._get_positional_encoding())
 
     # create a tensor of shape (seq_len, d_model)
     def _get_positional_encoding(self):
         pe = torch.zeros(self.seq_len, self.d_model)
         # create a tensor of shape (seq_len, 1)
         position = torch.arange(0, self.seq_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, self.d_model, 2) * (-math.log(10000.0) / self.d_model))
+        div_term = torch.exp(torch.arange(0, self.d_model, 2) * (-math.log(10000.0) / self.d_model)) # numerically stable
         # sin for even indices
         pe[:, 0::2] = torch.sin(position * div_term)
         # cos for odd indices
